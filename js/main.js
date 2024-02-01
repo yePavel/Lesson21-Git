520694779bfadbd00f3360961ded3447053ee7d6
pavel <pavel.yv@gmail.com>
2024-02-01 14:23:08 +0200

    Change ball color each time it is clicked

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@ var gBallSize = 100
 
 function onBallClick(elBall) {
     if (gBallSize >= 400) gBallSize = 50
+    const color = getRandomColor()
     const size = getRandomInt(20, 60)
 
     gBallSize += size
+
     elBall.style.height = gBallSize + 'px'
     elBall.style.width = gBallSize + 'px'
+    elBall.style.backgroundColor = `#${color}`
 
     elBall.innerText = gBallSize
 }
--- js/util.js
+++ js/util.js
@@ -3,3 +3,8 @@
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+function getRandomColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor
+}
